c12c9fc2fc4ee129110b3f2f1ed05f5a489d985f
WDCNelushan <nelushanwickramarachchi2002@gmail.com>
2024-12-15 13:05:06 +0530

    async await creation and env config

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,17 @@
+import { startSession } from "mongoose";
 import Product from "../models/product.js";
 
-export function getProduct(req,res){
-    Product.find().then(
-        (productList)=>{
+export async function getProduct(req,res){
+   try {
+    const productList = await Product.find()
             res.json({
                 list: productList
             })
-        }
-    ).catch((error)=>{
+        }catch(error){
         res.json({
             message : "Error"
         })
-    })
+    }
 }
 
 export function createProduct(req,res){
