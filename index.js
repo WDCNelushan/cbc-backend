dfa15fdb4d5ffb5301014d0b14b8aa0dc253371a
WDCNelushan <nelushanwickramarachchi2002@gmail.com>
2025-02-01 14:21:57 +0530

    env configuration

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ import mongoose from 'mongoose';
 import userRouter from './routes/userRouter.js';
 import productRouter from './routes/productRouter.js';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-const mongoUrl = "mongodb+srv://Chamika:wdcn2002@cluster0.lhdcl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongoUrl = process.env.MONGO_DB_URL;
 
 mongoose.connect(mongoUrl,{});
 const connection = mongoose.connection;
