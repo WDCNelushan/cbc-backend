cc818301324b6b08be0383834d8777e6c260b700
WDCNelushan <nelushanwickramarachchi2002@gmail.com>
2025-02-02 09:09:28 +0530

    create the product schema and model 😊

--- models/product.js
+++ models/product.js
@@ -1,9 +1,38 @@
 import mongoose from 'mongoose';
 
 const productSchema = mongoose.Schema({
-        name : String,
-        price : Number,
-        discription : String
+        
+    productId : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    productName : {
+        type : String,
+        required : true
+    },
+    altNames : [{
+        type : String
+    }],
+    images : [{
+        type : String
+    }],    
+    price : {
+        type : Number,
+        required : true
+    },
+    lastPrice : {
+        type : Number,
+        required : true
+    },
+    stock : {
+        type : Number,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    }
     })
 
 const Product = mongoose.model("products", productSchema);
