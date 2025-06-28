1bfe3f2aafdc216dffe4045c86b77d9fd4aeb997
WDCNelushan <nelushanwickramarachchi2002@gmail.com>
2025-06-28 17:44:03 +0530

    Hashed the password🔒🔒

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,18 @@
 import User from "../models/user.js";
+import bcrypt from 'bcrypt';
 
 export function CreateUser(req,res){
 
-    const user = new User(req.body)
+    const newUserData = new User(req.body);
+    newUserData.password = bcrypt.hashSync(newUserData.password,10);
+    const user = new User(newUserData);
 
     user.save().then(
         ()=>{
             res.json({
                 message: "User created"
             })
-            
+
         }).catch(()=>{
             res.json({
                 message: "User not created"
