7ac73fa792e03715f1bad57c3589f6f2f4c838fa
WDCNelushan <nelushanwickramarachchi2002@gmail.com>
2025-06-28 13:49:04 +0530

    create connection with database

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express from 'express'
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
+
+const mongoUrl = "mongodb+srv://admin:1234@cluster0.tiegsxe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(mongoUrl,{});
+const connection = mongoose.connection;
+
+connection.once("open",()=>{
+    console.log("Database connected")
+})
 
 app.use(bodyParser.json())
 
