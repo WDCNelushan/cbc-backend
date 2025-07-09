826769bccf5b46fd0bb54185853437374cb5151d
WDCNelushan <nelushanwickramarachchi2002@gmail.com>
2025-07-09 22:54:00 +0530

    take the latest product id 😊😊

---
+++ controllers/orderController.js
@@ -0,0 +1,25 @@
+import Order from "../models/order.js";
+
+export async function createOrder(req,res){
+
+    try{
+        const latestOrder = await Order.find().sort({date:-1}).limit(1)
+
+        let orderId;
+
+        if(latestOrder.length == 0){
+            orderId = "CBC0001"
+        }else{
+            const currentOrderId = latestOrder[0].orderId;
+            const numberString = currentOrderId.replace("CBC","");
+            const number = parseInt(numberString);
+            const newNumber = (number+1).toString().padStart(4,"0");
+            orderId = "CBC"+newNumber;
+        }
+
+    }catch(error){
+        res.status(500).json({
+            message : error.message
+        })
+    }
+}
