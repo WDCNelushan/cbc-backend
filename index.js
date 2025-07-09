ef7a41a8f65f43d007e45e7db916fb7a2709621b
WDCNelushan <nelushanwickramarachchi2002@gmail.com>
2025-07-09 23:02:15 +0530

    create order route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/userRouter.js';
 import jwt from 'jsonwebtoken';
 import productRouter from './routes/productRouter.js';
 import dotenv from 'dotenv';
+import orderRouter from './routes/orderRouter.js';
 dotenv.config();
 
 const app = express();
@@ -38,6 +39,7 @@ app.use((req,res,next)=>{
 
 app.use("/api/users",userRouter);
 app.use("/api/products",productRouter);
+app.use("/api/orders",orderRouter);
 
 app.listen(5000,
     ()=>{
